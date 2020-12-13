9880742ff3537937d89160f4a05d0cdea70f1944
Takahiro Kato <dozo13189@gmail.com>
2020-12-13 16:59:14 +0900

    use modifiers

--- src/components/SalutationName.vue
+++ src/components/SalutationName.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <select name="salutation" @change="$emit('update:salutation', $event.target.value)">
+        <select
+            name="salutation"
+            @change="updateSalutation"
+            >
+            <!-- @change="$emit('update:salutation', $event.target.value)" -->
+
         <option value="">-</option>
             <option
             v-for="item of salutations"
@@ -13,11 +18,12 @@
         </select>
 
         <input
-        :value="name"
-        @input="$emit('update:name', $event.target.value)"
-        type="text"
-        name="name"
-        />
+            :value="name"
+            @input="updateName"
+            type="text"
+            name="name"
+            />
+            <!-- @input="$emit('update:name', $event.target.value)" -->
         
     </div>
 </template>
@@ -32,20 +38,48 @@ const salutations = [
 ]
 
 export default {
-  props: {
-    salutation: {
-      type: String,
-      default: ''
+    props: {
+        salutation: {
+            type: String,
+            default: ''
+        },
+        salutationModifiers: {
+            default: () => ({}),
+            type: Object
+        },
+        name: {
+            type: String,
+            default: ''
+        },
+        nameModifiers: {
+            default: () => ({}),
+            type: Object
+        }
     },
-    name: {
-      type: String,
-      default: ''
+    setup (props, { emit }) {
+
+        const updateSalutation = event => {
+            let val = event.target.value
+            if (props.salutationModifiers.capitalize) {
+                val = val.toUpperCase()
+            }
+            emit('update:salutation', val)
+        }
+
+        const updateName = event => {
+            let val = event.target.value
+            if (props.nameModifiers.capitalize) {
+                val = val.charAt(0).toUpperCase() + val.slice(1)
+            }
+            emit('update:name', val)
+        }
+
+        return {
+            salutations,
+            updateSalutation,
+            updateName
+        }
     }
-  },
-  setup () {
-    return {
-      salutations
-    }
-  }
+
 }
 </script>
